1ad34d09bd832f9d0b377219bb7bb64d51d97afd
damienbod <damien_bod@hotmail.com>
2021-04-16 23:14:32 +0200

    clean up

--- MyApi/Startup.cs
+++ MyApi/Startup.cs
@@ -116,13 +116,11 @@ namespace MyApi
             {
                 var policy = new AuthorizationPolicyBuilder()
                     .RequireAuthenticatedUser()
-                   // .RequireClaim("email") // disabled this to test with users that have no email (no license added)
                     .Build();
                 options.Filters.Add(new AuthorizeFilter(policy));
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
