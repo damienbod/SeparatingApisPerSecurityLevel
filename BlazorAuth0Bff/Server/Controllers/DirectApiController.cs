5b99e443fc02f0b0d92bd5bf953d81d991989c79
damienbod <damien_bod@hotmail.com>
2021-04-17 08:22:40 +0200

    Direct data call clean up

--- BlazorAuth0Bff/Server/Controllers/DirectApiController.cs
+++ BlazorAuth0Bff/Server/Controllers/DirectApiController.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Net.Http;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -13,26 +11,9 @@ namespace BlazorAuth0Bff.Server.Controllers
     [Route("api/[controller]")]
     public class DirectApiController : ControllerBase
     {
-        private readonly MyApiServiceTwoClient _myApiClientService;
-        private readonly MyApiUserOneClient _myApiUserOneClient;
-
-        public DirectApiController(MyApiServiceTwoClient myApiClientService,
-            MyApiUserOneClient myApiUserOneClient)
-        {
-            _myApiClientService = myApiClientService;
-            _myApiUserOneClient = myApiUserOneClient;
-        }
-
         [HttpGet]
-        public async System.Threading.Tasks.Task<IEnumerable<string>> GetAsync()
+        public IEnumerable<string> GetAsync()
         {
-            // call service API
-            var serviceData = await _myApiClientService.GetServiceTwoApiData();
-
-            // call user API
-            string accessToken = await HttpContext.GetTokenAsync("access_token");
-            var userData = await _myApiUserOneClient.GetUserOneApiData(accessToken);
-
             return new List<string> { "some data", "more data", "loads of data" };
         }
     }
