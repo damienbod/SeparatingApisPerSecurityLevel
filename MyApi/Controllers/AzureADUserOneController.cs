3d009a93ffef1de2eee4411db0153a2ee53fcdec
damienbod <damien_bod@hotmail.com>
2021-11-27 11:41:04 +0100

    fix AAD API

--- MyApi/Controllers/AzureADUserOneController.cs
+++ MyApi/Controllers/AzureADUserOneController.cs
@@ -14,8 +14,9 @@ namespace MyApi.Controllers
     /// scope from App registration used to authorize.
     /// </summary>
     [SwaggerTag("API protected with Microsoft.Identity.Web and Azure AD")]
-    [AuthorizeForScopes(Scopes = new string[] { "api://72286b8d-5010-4632-9cea-e69e565a5517/user_impersonation" }, 
+    [AuthorizeForScopes(Scopes = new string[] { "api://b2a09168-54e2-4bc4-af92-a710a64ef1fa/access_as_user" }, 
         AuthenticationScheme = "myADscheme")]
+    [Authorize(AuthenticationSchemes = "myADscheme")]
     [ApiController]
     [Route("api/[controller]")]
     public class AzureADUserOneController : ControllerBase
